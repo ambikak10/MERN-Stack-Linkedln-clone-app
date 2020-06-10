6e9bf55d35c55a8f45c998aa51bdfcc957a0b29e
Ambika K <ambikak10@gmail.com>
2020-06-10 10:02:43 -0700

    auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -22,7 +22,7 @@ router.post(
     ).isLength({ min: 6 }),
   ],
   async (req, res) => {
-    console.log(req.body);
+    //consoleconsole.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.get('/', (req, res) => res.send('API Running'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/auth', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
 
