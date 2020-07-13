2331f2306a75c8804cabf7ac9a535228d41b72f8
Ambika K <ambikak10@gmail.com>
2020-07-13 15:09:17 -0700

    profile

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -6,4 +6,6 @@ export const USER_LOADED = "USER_LOADED";
 export const AUTH_ERROR = "AUTH_ERROR"; 
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS"; 
 export const LOGIN_FAIL = "LOGIN_FAIL"; 
-export const LOG_OUT = "LOGIN_OUT"; 
+export const LOG_OUT = "LOGIN_OUT";
+export const GET_PROFILE = "GET_PROFILE"; 
+export const PROFILE_ERROR = "PROFILE_ERROR"; 
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,3 +1,5 @@
+import { GET_PROFILE } from "../actions/types";
+
 const initialState = {
   profile: null,
   profiles: [],
@@ -5,4 +7,20 @@ const initialState = {
   loading: true,
   error:{}
 }
-export default function(state = initialState)
+export default function(state = initialState) {
+ const { type, payload } = action;
+ switch(type) {
+   case GET_PROFILE: 
+   return {
+     ...state,
+     profile: payload,
+     loading: false
+  }
+   case PROFILE_ERROR:
+     return {
+      ...state,
+      error: payload,
+      loading: false
+     }
+ }
+}
