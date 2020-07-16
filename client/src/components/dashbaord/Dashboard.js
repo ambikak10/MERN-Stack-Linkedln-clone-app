60607d78aceea91b901956b87bf3fedb59980546
Ambika K <ambikak10@gmail.com>
2020-07-15 19:22:08 -0700

    display exp edu

--- client/src/components/dashbaord/Dashboard.js
+++ client/src/components/dashbaord/Dashboard.js
@@ -1,30 +1,41 @@
 import React, {useEffect, Fragment} from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import  { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile, deleteAccount } from "../../actions/profile";
 import Spinner from '../layout/Spinner';
 import {Link} from 'react-router-dom';
 import DashboardActions from './DashboardActions';
+import Experience from "./Experience";
+import Education from "./Education";
 
 const Dashboard = ({ getCurrentProfile, auth: {user}, profile: {profile, loading}}) => {
   useEffect(() => {
     getCurrentProfile();
-  }, []);
-  return loading && profile === null ? <Spinner /> : <Fragment>
-    <h1 className='large text-primary'>Dashboard</h1>
-    <p className='lead'>
-      <i className='fas fa-user' /> Welcome {user && user.name}
-    </p>
-    {profile !== null ? (
+  }, [getCurrentProfile]);
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
     <Fragment>
-      <DashboardActions/>
-      </Fragment> ) : (
-      <Fragment>
-      <p>You have not setup a profile, please add some info</p>
-      <Link to='/create-profile' className="btn btn-primary my-1">Create profile</Link>
-      </Fragment>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <Fragment>
+          <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
+        </Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not setup a profile, please add some info</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            Create profile
+          </Link>
+        </Fragment>
       )}
-    </Fragment>;
+    </Fragment>
+  );
 }
 
 Dashboard.propTypes = {
