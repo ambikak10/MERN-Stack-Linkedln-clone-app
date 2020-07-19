12b97a47b7ed961e7f05c69071e6edadad0517c1
Ambika K <ambikak10@gmail.com>
2020-07-18 23:06:46 -0700

    post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -11,34 +11,50 @@ const PostItem = ({
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
 }) => (
-  <div class='post bg-white p-1 my-1'>
+  <div className='post bg-white p-1 my-1'>
     <div>
       <a href='profile.html'>
-        <img class='round-img' src={avatar} alt='' />
+        <img className='round-img' src={avatar} alt='' />
         <h4>{name}</h4>
       </a>
     </div>
     <div>
-      <p class='my-1'>{text}</p>
-      <p class='post-date'>
+      <p className='my-1'>{text}</p>
+      <p className='post-date'>
         Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
       </p>
-      <button type='button' class='btn btn-light'>
-        <i class='fas fa-thumbs-up'></i>
-<span>{likes.length}</span>
+      <button type='button' className='btn btn-light'>
+        <i className='fas fa-thumbs-up' />{" "}
+        <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
       </button>
-      <button type='button' class='btn btn-light'>
-        <i class='fas fa-thumbs-down'></i>
+      <button type='button' className='btn btn-light'>
+        <i className='fas fa-thumbs-down'></i>
       </button>
-      <a href='post.html' class='btn btn-primary'>
-Discussion <span class='comment-count'>{comments.length}</span>
-      </a>
-      <button type='button' class='btn btn-danger'>
-        <i class='fas fa-times'></i>
-      </button>
+      <Link to={`/post/${_id}`} className='btn btn-primary'>
+        Discussion
+        {comments.length > 0 && (
+          <span className='comment-count'>{comments.length}</span>
+        )}
+      </Link>
+      {!auth.loading && user === auth.user._id && (
+        <button type='button' className='btn btn-danger'>
+          <i className='fas fa-times'></i>
+        </button>
+      )}
     </div>
   </div>
 );
+PostItem.propTypes = {
+  post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  // addLike: PropTypes.func.isRequired,
+  // removeLike: PropTypes.func.isRequired,
+  // deletePost: PropTypes.func.isRequired,
+  // showActions: PropTypes.bool
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
 
-
-export default PostItem;
+export default connect(
+  mapStateToProps, {}) (PostItem);
